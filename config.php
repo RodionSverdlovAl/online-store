837c57230cec5dbdb7e536ba855400e89dcbccf8
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-08-21 19:58:58 +0300

    add configs and settings files

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+defined('VG_ACCESS') or die('access denied');
+
+const SITE_URL = 'http://online-store';
+const PATH ='/';
+
+
+// Data base
+
+const HOST = 'localhost';
+const USER ='root';
+const PASS = '';
+const DB_NAME = 'online-store';
+
